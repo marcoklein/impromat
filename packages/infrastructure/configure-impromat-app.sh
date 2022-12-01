1074515cc9376028592d453a10518cc3df1fee6d
Marco Klein <hello@marcoklein.dev>
2022-12-01 20:42:16 +0100

    ci: dev environment

--- packages/infrastructure/configure-impromat-app.sh
+++ packages/infrastructure/configure-impromat-app.sh
@@ -3,16 +3,23 @@
 export WORKDIR=$(cd $(dirname $0) && pwd)
 source $WORKDIR/common.sh
 
-appName=impromat-app-production
-domain="impromat.app"
+environmentName=$1
+domain="$2"
+apiUrl=$3
+
+appPrefix=impromat-app
+appName=$appPrefix-$environmentName
 dockerfilePath="packages/infrastructure/Dockerfile-impromat-app"
+echo "appPrefix=$appPrefix"
+echo "appName=$appName"
+echo "domain=$domain"
 
 ensureAppExists $appName
 setCommonDockerOptions $appName $dockerfilePath
 configureDomain $appName $domain
 
 log "Set App Configuration"
-dokku config:set --no-restart $appName REACT_APP_API_URL=https://api.impromat.app
+dokku config:set --no-restart $appName REACT_APP_API_URL=https://$apiUrl
 
 log "Set Docker Options"
-dokku docker-options:add $appName build "--build-arg REACT_APP_API_URL=https://api.impromat.app"
+dokku docker-options:add $appName build "--build-arg REACT_APP_API_URL=https://$apiUrl"
