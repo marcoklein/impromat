55f5f0781d42952834f7a8ac9150c777fc0ef3eb
Marco Klein <hello@marcoklein.dev>
2022-12-15 18:09:24 +0100

    ci: development and production configuration

--- packages/infrastructure/scripts/configure-impromat-app.sh
+++ packages/infrastructure/scripts/configure-impromat-app.sh
@@ -4,7 +4,7 @@ export WORKDIR=$(cd $(dirname $0) && pwd)
 source $WORKDIR/common.sh
 
 environmentName=$1
-domain="$2"
+domains=$2
 apiUrl=$3
 
 appPrefix=impromat-app
@@ -13,11 +13,11 @@ dockerfilePath="packages/infrastructure/Dockerfile-impromat-app"
 echo "environmentName=$environmentName"
 echo "appPrefix=$appPrefix"
 echo "appName=$appName"
-echo "domain=$domain"
+echo "domains=$domains"
 
 ensureAppExists $appName
 setCommonDockerOptions $appName $dockerfilePath
-configureDomain $appName $domain
+configureDomain $appName "$domains"
 
 log "Set App Configuration"
 dokku config:set --no-restart $appName REACT_APP_API_URL=https://$apiUrl
@@ -25,13 +25,3 @@ dokku config:set --no-restart $appName REACT_APP_API_URL=https://$apiUrl
 log "Set Docker Options"
 dokku docker-options:clear $appName build
 dokku docker-options:add $appName build "--build-arg REACT_APP_API_URL=https://$apiUrl"
-
-if [ "$environmentName" = "development" ]
-then
-  echo "Adding development configuration"
-  packageVersion=$(cd $WORKDIR/../impromat && echo $(cat ./package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]'))
-  echo "packageVersion=$packageVersion"
-  appVersion="$packageVersion-$(($RANDOM % 999))"
-  echo "appVersion=$appVersion"
-  dokku docker-options:add $appName build "--build-arg REACT_APP_VERSION=$packageVersion-$RANDOM"
-fi
