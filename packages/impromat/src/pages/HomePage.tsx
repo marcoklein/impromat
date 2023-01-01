0f757c8aea18f7bbb2ee664780af26e8be2c1cf6
maschamiriam <83670301+maschamiriam@users.noreply.github.com>
2023-01-01 19:11:44 +0100

    adding text and format changes on home page

--- packages/impromat/src/pages/HomePage.tsx
+++ packages/impromat/src/pages/HomePage.tsx
@@ -5,12 +5,17 @@ import {
   IonImg,
   IonMenuToggle,
   IonPage,
+  IonRouterLink,
   IonText,
   ScrollCustomEvent,
 } from "@ionic/react";
 import { arrowForwardOutline, information, menu } from "ionicons/icons";
 import { useState } from "react";
-import { routeAbout, routeWorkshops } from "../routes/shared-routes";
+import {
+  routeAbout,
+  routePrivacyPolicy,
+  routeWorkshops,
+} from "../routes/shared-routes";
 import { Tabs } from "./library/components/LibraryContentComponent";
 import { routeLibrary, routeLibraryTab } from "./library/library-routes";
 
@@ -60,7 +65,7 @@ export const HomePage: React.FC = () => {
             <IonText color="dark">
               <h1 style={{ fontSize: "2rem" }}>Impromat</h1>
             </IonText>
-            <IonText color="medium">
+            <IonText color="dark">
               <h1>
                 App for planning, giving, and sharing improvisational theatre
                 workshops.
@@ -73,18 +78,19 @@ export const HomePage: React.FC = () => {
             <IonText color="dark">
               <h1>Improving Improv</h1>
             </IonText>
-            <IonText color="medium">
-              Developed by improvisers - made for improvisers. Impromat offers
-              all necessary features to plan upcoming improv sessions.
+            <IonText color="dark">
+              <b>Developed by improvisers — made for improvisers.</b> Impromat
+              offers all necessary features to plan your upcoming improv
+              sessions.
             </IonText>
             <IonText color="dark">
               <h3>Improv Workshops</h3>
             </IonText>
-            <IonText color="medium">
+            <IonText color="dark">
               <p>
-                Plan and manage your own improvisational theatre workshops,
-                share them with the community, or find inspiring workshops from
-                other improvisers.
+                <b>Plan and manage</b> your own improvisational theatre
+                workshops, <b>share</b> them with the community or <b>find</b>{" "}
+                inspiring workshops from other improvisers.
               </p>
               <IonButton
                 fill="solid"
@@ -95,23 +101,27 @@ export const HomePage: React.FC = () => {
                 Create a Workshop
               </IonButton>
               <p>
-                Improv workshops often have an overall topic and are built up by
-                sections (e.g. warm-up), in which different elements (exercises
-                and games) are ordered. You can add your own notes to the
-                workshop as well as to every single element to adapt exercices
-                to your workshop topic and style. For optimal time keeping
-                during the workshop, you can specify the timeframe in advance
-                and use the presentation mode.
+                Improv workshops often have an overall <b>topic</b> and are
+                built up by <b>sections</b> (e.g. warm-up), in which different
+                elements (exercises and games) are ordered. You can add your own{" "}
+                <b>notes</b> to the workshop as well as to every single element
+                to adapt the exercises to your workshop topic and style.
+              </p>
+              <p>
+                For optimal time keeping, you can specify the <b>timeframe</b>{" "}
+                of every element in advance and use the <b>presentation mode</b>{" "}
+                during the workshop.
               </p>
             </IonText>
 
             <IonText color="dark">
               <h3>Improv Library</h3>
             </IonText>
-            <IonText color="medium">
+            <IonText color="dark">
               <p>
                 Explore over 1000 existing improv exercises and games in the{" "}
-                <b>Element Library</b> from various sources like{" "}
+                <b>Element Library</b>. They have beeen extracted from different{" "}
+                <b>wiki sources</b> in German and English:{" "}
                 <a
                   href="https://improwiki.com"
                   target="_blank"
@@ -126,41 +136,60 @@ export const HomePage: React.FC = () => {
                   rel="noreferrer"
                 >
                   Learnimprov
-                </a>
-                , and{" "}
+                </a>{" "}
+                and{" "}
                 <a
                   href="https://wiki.improvresourcecenter.com"
                   target="_blank"
                   rel="noreferrer"
                 >
-                  Improvresourcecenter
+                  Improv resource center
                 </a>
-                . Use the Recommendations to find similiar elements to add to
-                your workshop.
+                . Use the <b>Recommendations</b> feature to find similiar
+                elements to add to your workshop.
               </p>
             </IonText>
-            <IonButton fill="outline" routerLink={routeLibrary()}>
+            <IonButton
+              fill="solid"
+              color="tertiary"
+              routerLink={routeLibrary()}
+            >
               <IonIcon slot="start" icon={arrowForwardOutline}></IonIcon>
               Open Element Library
             </IonButton>
             <IonText color="dark">
               <h3>Customize Your Improv</h3>
             </IonText>
-            <IonText color="medium">
+            <IonText color="dark">
               <p>
-                Tag certain elements, e.g. exercices you commonly use in your
-                workshops, as favorites for easy access. If you cannot find an
-                exercise or game in the library, create your own element and
-                incorporate them in you workshop(s).
+                For easy access, tag certain elements, e.g. exercises you
+                commonly use in your workshops, as <b>Favorites</b>. If you
+                cannot find an exercise or game in the library, <b>create</b>{" "}
+                your own element and incorporate them in your workshop(s).
               </p>
             </IonText>
             <IonButton
-              fill="outline"
+              fill="solid"
+              color="danger"
               routerLink={routeLibraryTab(Tabs.FAVORITES)}
             >
               <IonIcon slot="start" icon={arrowForwardOutline}></IonIcon>
               Open Favorite Elements
             </IonButton>
+            <p>
+              The <b>Login</b> via Google account allows you to{" "}
+              <b>synchronize</b> your workshops, favorite elements and
+              personally created elements on all devices. Thus, you can plan a
+              workshop on your computer with the web version of Impromat and use
+              the <b>Desktop app</b> on your smartphone during the workshop.
+              (Don't worry, your improv data is not shared with Google and
+              safely hosted on a server based in Germany — the Impromat server
+              does not store any login or personal information. See our{" "}
+              <IonRouterLink routerLink={routePrivacyPolicy()}>
+                Privacy Policy
+              </IonRouterLink>{" "}
+              for more information.)
+            </p>
           </div>
         </div>
         <div style={{ display: "flex", justifyContent: "center" }}>
@@ -169,44 +198,60 @@ export const HomePage: React.FC = () => {
               <h1>Made For Improvisers</h1>
             </IonText>
 
-            <IonText color="medium">
-              Impromat is made for all kind of levels: Improv beginners who want
+            <IonText color="dark">
+              Impromat is made for <b>all levels</b>: Improv beginners who want
               to prepare their first improv workshops, established groups that
               want further inspiration and experienced workshop leaders who look
-              for a one-stop app for managing their regular improv workshops.
+              for a one-stop app for managing their improv workshops.{" "}
+              <b>Try it out!</b>
             </IonText>
             <IonText color="dark">
               <h3>Improv Beginners</h3>
             </IonText>
-            <IonText color="medium">
+            <IonText color="dark">
               <p>
-                Are you new to improv and want to try it out with some people?
-                Impromat helps you in finding exercises and games for your first
-                improv session.
+                Are you new to improvisational theatre and want to try it out
+                with some people? Impromat <b>guides</b> you in finding
+                exercises and games for your first improv session. Use the
+                recommended elements or already existing workshops to benefit
+                from the <b>collective improv knowledge</b>.
               </p>
             </IonText>
             <IonText color="dark">
               <h3>Established Groups</h3>
             </IonText>
-            <IonText color="medium">
+            <IonText color="dark">
               <p>
-                Are you playing in an improv group and you are looking for new
-                inspiration for your next workshop? Impromat helps you with
-                finding new exercices and games around certain topics.
+                Are you playing in an improv group and want to{" "}
+                <b>discover new games</b> together? Or do you want to bring more{" "}
+                <b>structure</b> into your regular sessions by choosing{" "}
+                <b>specific topics</b>
+                (e.g. character building, focus) to work on with your group.
+                Impromat helps you with finding new exercises and games around
+                certain topics. It also allows you to arrange the elements in
+                sections to have a ready-planned workshop in no time.
               </p>
             </IonText>
             <IonText color="dark">
               <h3>Workshop Leaders</h3>
             </IonText>
-            <IonText color="medium">
+            <IonText color="dark">
               <p>
                 Are you regularly giving improv workshops within your own group
-                or for others? With Impromat you can easily plan, hold, and
-                share your workshops.
+                or for others? With Impromat you can easily{" "}
+                <b>plan, guide through and manage your workshops</b>,{" "}
+                <i className="a"></i> by duplicating and adapting given workshop
+                structures, adding your personal favorites and creating your own
+                elements — plus, when participants or other workshop leaders ask
+                you for your notes on a workshop, you can <b>share</b> your
+                Impromat workshop with them in no time.
+              </p>
+              <p>
+                <b>Let Impromat be your improv tool!</b>
               </p>
             </IonText>
             <IonButton
-              fill="outline"
+              fill="solid"
               routerLink={routeWorkshops()}
               className="ion-margin-top"
             >
@@ -217,15 +262,19 @@ export const HomePage: React.FC = () => {
             <IonText color="dark">
               <h3>You Want More?</h3>
             </IonText>
-            <IonText color="medium">
+            <IonText color="dark">
               <p>
-                Impromat lives from your workshop and element contributions. If
-                you have feedback or new ideas, what the Impromat could do,
-                please let me knwo via email at{" "}
+                Impromat and the improv community thrive from your workshop and
+                element <b>contributions</b>. Everyone can take part in sharing
+                their improv knowledge!
+              </p>
+              <p>
+                If you have <b>feedback or new ideas</b>, what the Impromat
+                could do, please let us know via email at{" "}
                 <a href="mailto:impromat@marcoklein.dev">
                   impromat@marcoklein.dev
                 </a>{" "}
-                or head over to the about page to find out more.
+                or head over to the <b>About</b> page to find out more.
               </p>
             </IonText>
             <IonButton fill="outline" routerLink={routeAbout()}>
