25e4430c8bd0603bdcc8e7b10623216a5b40e5a7
Marco Klein <hello@marcoklein.dev>
2023-01-08 22:42:41 +0100

    test: add empty test

---
+++ packages/improbib-experiments/test/test.test.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import { describe } from "mocha";
+
+describe("empty test", () => {
+  it("should be empty", () => {
+    // empty
+    expect(true).to.be.true;
+  });
+});
