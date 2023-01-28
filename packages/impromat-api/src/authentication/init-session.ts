e93711e67dedfed03b39c039d6086655834f1f6f
Marco Klein <hello@marcoklein.dev>
2023-01-28 09:13:14 +0100

    fix: cleare session cookie on logout

--- packages/impromat-api/src/authentication/init-session.ts
+++ packages/impromat-api/src/authentication/init-session.ts
@@ -1,13 +1,16 @@
 import { Express } from "express";
 import session, { SessionOptions } from "express-session";
+import path from "node:path";
 import sessionFileStore from "session-file-store";
 import { v4 as uuidv4 } from "uuid";
 import { environment } from "../environment";
-import path from "node:path";
 const FileStore = sessionFileStore(session);
+
+export const SESSION_COOKIE_NAME = "connect.sid";
 
 export function initExpressSessions(app: Express) {
   const sessionOptions: SessionOptions = {
+    name: SESSION_COOKIE_NAME,
     genid: () => uuidv4(),
     store: new FileStore({
       path: path.join(environment.STORAGE_PATH, "sessions"),
