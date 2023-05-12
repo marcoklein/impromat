9278a5b7d3e608918fd9c9eea36d7fd67d715546
Marco Klein <hello@marcoklein.dev>
2023-05-12 15:32:20 +0200

    fix: vite build

--- packages/impromat/test/e2e/home-page.test.ts
+++ packages/impromat/test/e2e/home-page.test.ts
@@ -1,8 +1,8 @@
 import { expect } from "@playwright/test";
-import { pageTest } from "../component/fixtures/page-fixtures.js";
 import { e2ePageTest } from "./fixtures/e2e-page-fixtures.js";
+import { pageTest } from "../component/fixtures/page-fixtures.js";
 
-pageTest("Home Page", () => {
+pageTest.describe("Home Page", () => {
   e2ePageTest("should render home page.", async ({ homePage }) => {
     // given
     // when
