6865528c7c8cd922048c33d493d073fc8ef086ca
Marco Klein <hello@marcoklein.dev>
2023-09-25 18:52:20 +0200

    chore: remove unused impromat next lib (#279)

--- packages/demonstrators/impromat-app-next/src/components/LibraryElementList/LibraryElementList.styles.ts
+++
@@ -1,8 +0,0 @@
-import { createStyles } from '@mantine/core';
-
-export default createStyles((theme) => ({
-  like: {
-    // TODO switch color index for dark and light automatically
-    color: theme.colors.red[theme.colorScheme === 'dark' ? 7 : 3],
-  },
-}));
