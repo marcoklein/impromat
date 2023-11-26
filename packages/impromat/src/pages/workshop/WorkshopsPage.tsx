9edfd6f6ddb0b01f9d80556bd68ac32bdb0c215b
Marco Klein <hello@marcoklein.dev>
2023-11-26 21:41:07 +0100

    feat: refresher

--- packages/impromat/src/pages/workshop/WorkshopsPage.tsx
+++ packages/impromat/src/pages/workshop/WorkshopsPage.tsx
@@ -1,7 +1,9 @@
 import { IonButton, IonContent, IonFab, IonIcon, IonLabel } from "@ionic/react";
 import { filter } from "ionicons/icons";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { useQuery } from "urql";
+import { PageContentLoaderComponent } from "../../components/PageContentLoaderComponent";
 import { PageScaffold } from "../../components/PageScaffold";
 import { VirtualCardGrid } from "../../components/VirtualCardGrid";
 import { WorkshopsFilterBar } from "../../components/WorkshopsFilterBar";
@@ -15,7 +17,6 @@ import { usePersistedState } from "../../hooks/use-persisted-state";
 import { useStateChangeLogger } from "../../hooks/use-state-change-logger";
 import { WorkshopCreateFirstComponent } from "./components/WorkshopCreateFirstComponent";
 import { WorkshopPreviewCard } from "./components/WorkshopPreviewCard";
-import { useTranslation } from "react-i18next";
 
 const WorkshopFields_WorkshopFragment = graphql(`
   fragment WorkshopFields_Workshop on Workshop {
@@ -41,7 +42,6 @@ const WorkshopsQuery = graphql(`
 export const WorkshopsPage: React.FC = () => {
   const logger = useComponentLogger("WorkshopsPage");
 
-  const context = useMemo(() => ({ additionalTypenames: ["Workshop"] }), []);
   const { myUserId } = useIsLoggedIn();
 
   const defaultFilterInput: Required<UserWorkshopsFilterInput> = useMemo(
@@ -68,7 +68,6 @@ export const WorkshopsPage: React.FC = () => {
 
   const [workshopsQueryResult, reexecuteWorkshopsQuery] = useQuery({
     query: WorkshopsQuery,
-    context,
     pause: !myUserId,
     variables: {
       userId: myUserId!,
@@ -84,8 +83,11 @@ export const WorkshopsPage: React.FC = () => {
 
   const availableWorkshops = getFragmentData(
     WorkshopFields_WorkshopFragment,
-    workshopsQueryResult.data?.user.workshops,
+    workshopsQueryResult.data?.user?.workshops,
   );
+
+  const [gridIsOnTop, setGridIsOnTop] = useState<boolean>(false);
+  useStateChangeLogger(gridIsOnTop, "gridIsOnTop", logger);
 
   const presentWorkshopInputDialog = useCreateWorkshopInputDialog();
 
@@ -122,52 +124,60 @@ export const WorkshopsPage: React.FC = () => {
             <IonLabel>{t("NewWorkshop")}</IonLabel>
           </IonButton>
         </IonFab>
-        {availableWorkshops?.length ? (
-          <VirtualCardGrid
-            scrollStoreKey="workshops-page"
-            isFetching={
-              workshopsQueryResult.fetching || workshopsQueryResult.stale
-            }
-            items={availableWorkshops}
-            itemContent={(_index, workshop) => (
-              <WorkshopPreviewCard
-                workshopFragment={workshop}
-              ></WorkshopPreviewCard>
-            )}
-          ></VirtualCardGrid>
-        ) : !userWorkshopsFilterInput.liked ||
-          !userWorkshopsFilterInput.owned ? (
-          <div
-            className="ion-padding"
-            style={{
-              minHeight: "100%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <div>
-              <p>{t("FilterNoWorkshops")}</p>
-              <IonButton
-                expand="full"
-                onClick={() => {
-                  setUserWorkshopsFilterInput({
-                    liked: true,
-                    owned: true,
-                    isPublic: true,
-                  });
-                }}
-              >
-                <IonIcon slot="start" icon={filter}></IonIcon>
-                {t("ClearFilters")}
-              </IonButton>
+
+        <PageContentLoaderComponent
+          noRefresher={!gridIsOnTop}
+          queryResult={workshopsQueryResult}
+          reexecuteQuery={reexecuteWorkshopsQuery}
+        >
+          {availableWorkshops?.length ? (
+            <VirtualCardGrid
+              scrollStoreKey="workshops-page"
+              isFetching={
+                workshopsQueryResult.fetching || workshopsQueryResult.stale
+              }
+              onTopStateChange={(atTop) => setGridIsOnTop(atTop)}
+              items={availableWorkshops}
+              itemContent={(_index, workshop) => (
+                <WorkshopPreviewCard
+                  workshopFragment={workshop}
+                ></WorkshopPreviewCard>
+              )}
+            ></VirtualCardGrid>
+          ) : !userWorkshopsFilterInput.liked ||
+            !userWorkshopsFilterInput.owned ? (
+            <div
+              className="ion-padding"
+              style={{
+                minHeight: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <div>
+                <p>{t("FilterNoWorkshops")}</p>
+                <IonButton
+                  expand="full"
+                  onClick={() => {
+                    setUserWorkshopsFilterInput({
+                      liked: true,
+                      owned: true,
+                      isPublic: true,
+                    });
+                  }}
+                >
+                  <IonIcon slot="start" icon={filter}></IonIcon>
+                  {t("ClearFilters")}
+                </IonButton>
+              </div>
             </div>
-          </div>
-        ) : (
-          <WorkshopCreateFirstComponent
-            onCreateWorkshopClick={() => presentWorkshopInputDialog()}
-          ></WorkshopCreateFirstComponent>
-        )}
+          ) : (
+            <WorkshopCreateFirstComponent
+              onCreateWorkshopClick={() => presentWorkshopInputDialog()}
+            ></WorkshopCreateFirstComponent>
+          )}
+        </PageContentLoaderComponent>
       </IonContent>
     </PageScaffold>
   );
