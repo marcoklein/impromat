c9db1a998a755970f67acee837f0de7d4f98ec9c
Marco Klein <hello@marcoklein.dev>
2024-05-22 22:49:19 +0200

    fix: enable debug logging

---
+++ .changeset/twenty-paws-turn.md
@@ -0,0 +1,5 @@
+---
+'impromat-api': patch
+---
+
+Enable debug logging
--- packages/impromat-app-renderer/src/web-browser.ts
+++ packages/impromat-app-renderer/src/web-browser.ts
@@ -1,4 +1,4 @@
-import puppeteer, { Browser } from "puppeteer";
+import puppeteer, { Browser } from 'puppeteer';
 
 export class WebBrowser {
   private browser: Browser | undefined;
@@ -11,38 +11,49 @@ export class WebBrowser {
    * @returns The HTML of the page.
    */
   async getPageHtml(url: string) {
-    const browser = await this.getBrowser();
-    const page = await browser.newPage();
-    await page.goto(url, {
-      waitUntil: "networkidle0",
-    });
-    const pageContent = await page.content();
-    await this.releaseBrowser();
-
-    return pageContent;
+    try {
+      const browser = await this.getBrowser();
+      const page = await browser.newPage();
+      try {
+        await page.goto(url, {
+          waitUntil: 'networkidle0',
+        });
+        return await page.content();
+      } catch (error) {
+        console.error('Error getting page content', error);
+      } finally {
+        console.log('Closing page');
+        await page.close();
+      }
+    } catch (error) {
+      console.error('Error getting page content', error);
+    } finally {
+      await this.releaseBrowser();
+    }
   }
 
   private async getBrowser() {
     if (!this.browser) {
       this.browser = await puppeteer.launch({
         // no need for sandbox as we only serve static files from the impromat app
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
-        headless: "new",
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        headless: 'new',
       });
-      console.log("Created new browser");
+      console.log('Created new browser');
     }
     this.browserLocks++;
-    console.log("getBrowser Locks: ", this.browserLocks);
+    console.log('getBrowser Locks: ', this.browserLocks);
     return this.browser;
   }
 
   private async releaseBrowser() {
     this.browserLocks--;
-    console.log("releaseBrowser Locks: ", this.browserLocks);
+    console.log('releaseBrowser Locks: ', this.browserLocks);
     if (this.browserLocks <= 0) {
+      console.log('Closing browser');
       await this.browser?.close();
       this.browser = undefined;
-      console.log("Closed browser");
+      console.log('Closed browser');
     }
   }
 }
