550ec428c05844e25075d2bf5655fdd456e9d41d
Marco Klein <hello@marcoklein.dev>
2024-05-22 09:27:11 +0200

    fix: dist build folder (#468)

--- packages/impromat-app-renderer/src/start-prerender-app.ts
+++ packages/impromat-app-renderer/src/start-prerender-app.ts
@@ -1,7 +1,7 @@
-import { load } from "cheerio";
-import express, { Request, Response } from "express";
-import { DynamicSitemap } from "./dynamic-sitemap";
-import { WebBrowser } from "./web-browser";
+import { load } from 'cheerio';
+import express, { Request, Response } from 'express';
+import { DynamicSitemap } from './dynamic-sitemap';
+import { WebBrowser } from './web-browser';
 
 const cache = new Map<string, string>();
 
@@ -22,32 +22,32 @@ export function startPrerenderApp(
   const app = express();
 
   // sitemap.txt
-  app.get("/sitemap.txt", async (req: Request, res: Response) => {
-    console.log("sitemap.txt Request: ", req.url);
+  app.get('/sitemap.txt', async (req: Request, res: Response) => {
+    console.log('sitemap.txt Request: ', req.url);
     const sitemapContent = await sitemap.getSitemapTxt();
     // send text file
-    res.setHeader("Content-Type", "text/plain");
+    res.setHeader('Content-Type', 'text/plain');
     res.send(sitemapContent);
   });
 
   // attach all static files to the express server
-  app.use(express.static("../impromat/build", { index: false }));
+  app.use(express.static('../impromat/dist', { index: false }));
 
   // serve all static files directly (e.g. images, fonts, etc.)
-  app.get("*.*", (req: Request, res: Response) => {
-    console.log("Static file Request: ", req.url);
+  app.get('*.*', (req: Request, res: Response) => {
+    console.log('Static file Request: ', req.url);
     res.sendFile(req.url);
   });
 
   // prerender all html pages (e.g. no file extension)
-  app.get("*", async (req: Request, res: Response) => {
-    console.log("Dynamic page request: ", req.url);
+  app.get('*', async (req: Request, res: Response) => {
+    console.log('Dynamic page request: ', req.url);
     const html = await handlePageRequest(req.url);
     res.send(html);
   });
 
   async function handlePageRequest(url: string) {
-    console.log("cache size: ", cache.size);
+    console.log('cache size: ', cache.size);
     if (cache.has(url)) {
       return cache.get(url);
     }
@@ -55,7 +55,7 @@ export function startPrerenderApp(
       `http://localhost:${clientPort}${url}`
     );
     const $ = load(pageHtml);
-    $("head style").remove();
+    $('head style').remove();
     const html = $.html();
     cache.set(url, html);
     return html;
